829ff508ca59e8fe3c9ba3c9ef0af421f75f6e03
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-23 02:47:11 +0330

    server

--- MyServer/Hubs/StreamHub.cs
+++ MyServer/Hubs/StreamHub.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +11,37 @@ namespace MyServer.Hubs
     {
         public async IAsyncEnumerable<string> SendDataRow()
         {
-            for (int i = 0; i < 10; i++)
+            string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mypub;Integrated Security=True";
+
+            string query = "SELECT * FROM Payments";
+
+            using SqlConnection connection =
+            new SqlConnection(connectionString);
+            // Create the Command and Parameter objects.
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            // Open the connection in a try/catch block. 
+            // Create and execute the DataReader, writing the result
+            // set to the console window.
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
             {
+                Console.WriteLine("\t{0}\t{1}\t{2}",
+                    reader[0], reader[1], reader[2]);
+
                 await Task.Delay(1000);
-                yield return i.ToString();
+                yield return reader[1].ToString();
             }
+            reader.Close();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    await Task.Delay(1000);
+            //    yield return i.ToString();
+            //}
         }
         
     }
