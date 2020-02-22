40644a2d0e122aa2f0238f88b91d2921afc11bdf
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-23 02:22:24 +0330

    Add project files.

---
+++ MyClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyClient
+{
+    class Program
+    {
+        private static HubConnection connection;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            connection = new HubConnectionBuilder()
+              .WithUrl("http://localhost:5000/streamHub")
+              .ConfigureLogging(logging =>
+              {
+                  logging.AddDebug();
+                  logging.SetMinimumLevel(LogLevel.Debug);
+              })
+              .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(1000);
+                await connection.StartAsync();
+            };
+
+            await connection.StartAsync();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var stream = connection.StreamAsync<string>("SendDataRow", cancellationTokenSource.Token);
+
+            await foreach (var count in stream)
+            {
+                Console.WriteLine($"{count}");
+            }
+
+            Console.WriteLine("Streaming completed");          
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyServer/Hubs/StreamHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyServer.Hubs
+{
+    public class StreamHub : Hub
+    {
+        public async IAsyncEnumerable<string> SendDataRow()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(100);
+                yield return i.ToString();
+            }
+        }
+        
+    }
+}
